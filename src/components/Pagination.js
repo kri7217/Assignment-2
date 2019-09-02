693d34b814e38ae4b067df5cd9104f212d25d19d
kri7217 <krishnaar4@gmail.com>
2019-09-03 02:04:50 +0530

    Update Pagination.js

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -10,14 +10,12 @@ class Pagination extends Component {
     }
 
     componentWillMount() {
-        // set page if items array isn't empty
         if (this.props.items && this.props.items.length) {
             this.setPage(this.props.initialPage);
         }
     }
 
     componentDidUpdate(prevProps, prevState) {
-        // reset page if items array has changed
         if (this.props.items !== prevProps.items) {
             this.setPage(this.props.initialPage);
         }
@@ -31,15 +29,15 @@ class Pagination extends Component {
             return;
         }
 
-        // get new pager object for specified page
+        //  new pager object 
         pager = this.getPager(items.length, page);
 
         // get new page of items from items array
         var pageOfItems = items.slice(pager.startIndex, pager.endIndex + 1);
-        // update state
+        
         this.setState({ pager: pager });
 
-        // call change page function in parent component
+        
         this.props.onChangePage(pageOfItems);
     }
 
@@ -55,11 +53,9 @@ class Pagination extends Component {
 
         var startPage, endPage;
         if (totalPages <= 10) {
-            // less than 10 total pages so show all
             startPage = 1;
             endPage = totalPages;
         } else {
-            // more than 10 total pages so calculate start and end pages
             if (currentPage <= 6) {
                 startPage = 1;
                 endPage = 10;
@@ -72,11 +68,11 @@ class Pagination extends Component {
             }
         }
 
-        // calculate start and end item indexes
+        
         var startIndex = (currentPage - 1) * pageSize;
         var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
 
-        // create an array of pages to ng-repeat in the pager control
+        
         var pages = [...Array((endPage + 1) - startPage).keys()].map(i => startPage + i);
 
         let r={
@@ -92,7 +88,6 @@ class Pagination extends Component {
         }
 
         console.log(r)
-        // return object with all pager properties required by the view
         return {
             totalItems: totalItems,
             currentPage: currentPage,
